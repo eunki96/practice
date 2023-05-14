1dbd7640512aaf248687da9718fdc688e874cdef
Eungi Jeong <eungi9640@gmail.com>
2023-05-15 00:20:34 +0900

    Update Main.java

--- src/main/java/aim_to_platinum/backtracking/b17136/Main.java
+++ src/main/java/aim_to_platinum/backtracking/b17136/Main.java
@@ -57,6 +57,7 @@ public class Main {
     public static boolean isAttachable(int x, int y, int size){
         for(int i=x; i<(x+size); i++){
             for(int j=y; j<(y+size); j++){
+
                 if(i<0 || j<0 || i>9 || j>9){
                     return false;
                 }
