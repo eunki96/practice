6ad8b0752334d5fe5b070059ebb70679eeb5f54c
Eungi Jeong <eungi9640@gmail.com>
2023-05-26 05:00:09 +0900

    update

---
+++ src/main/java/aim_to_platinum/week02_divide_and_conquer/b14600/Main.java
@@ -0,0 +1,11 @@
+package aim_to_platinum.week02_divide_and_conquer.b14600;
+/*
+https://rebro.kr/64
+2^n X 2^n 크기의 정사각형은 1X1 한 칸을 제외하고 ㄴ 모양의 타일로 전부 채울 수 있다.
+
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
