19736b882adee09bde5fa56b8b11f2181380b87b
Eungi Jeong <eungi9640@gmail.com>
2023-07-17 05:21:45 +0900

    update

--- src/main/java/aim_to_platinum/week07_tree/b3584/BOJ_3584_가장_가까운_공통_조상.java
+++ src/main/java/aim_to_platinum/week07_tree/b3584/BOJ_3584_가장_가까운_공통_조상.java
@@ -1,7 +1,28 @@
 package aim_to_platinum.week07_tree.b3584;
+/*
+1. 문제 요약
+- 공통 조상에 대한 정의는 다음과 같다
+    두 노드를 모두 자손으로 가지면서 깊이가 가장 깊은 노드
+- 테스트 케이스의 수 T를 입력받는다
+    --- 각 테스트 케이스 ---
+    노드의 수 N(2<=N<=10000)을 입력받는다
+    N-1 번에 걸쳐 간선 정보가 주어진다
+        A B 입력을 받을 경우 A는 B의 부모노드임을 뜻한다
+    이후 가장 가까운 공통조상을 구할 두 노드를 입력받는다
+
+2. 아이디어 (문제 접근법)
+[아이디어-1]
+-
+
+3. 어려움 및 해결
+-
+ */
+
+
+import java.io.IOException;
 
 public class BOJ_3584_가장_가까운_공통_조상 {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException {
+
     }
 }
